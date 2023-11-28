039e26691a09ce8c42ad09aaab94e91dadf4c245
Rick W <wurick100@gmail.com>
2023-11-29 10:47:53 +1100

    update agent output

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
                 
                 //def name = currentBuild.agent.name
                 
-                // echo "Building... ${currentBuild.agent.workspace}"
+                echo "Building... ${currentBuild.agent.name}"
                 }
             }
         }
