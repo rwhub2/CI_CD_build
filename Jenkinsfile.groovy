412d71c12f366b7729984415f8b36c331fa2f3c5
Rick W <wurick100@gmail.com>
2023-11-29 10:51:05 +1100

    update env variables

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
                 
                 //def name = currentBuild.agent.name
                 
-                echo "Building... ${currentBuild.agent.name}"
+                echo "Building... ${env.NODE_NAME}"
                 }
             }
         }
