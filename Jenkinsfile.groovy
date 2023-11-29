ffc8174f002e29633de2aba04029218d71dace1c
Rick W <wurick100@gmail.com>
2023-11-29 14:11:25 +1100

    update the py file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ pipeline {
                 
                 echo "Building...${env.Build_ID}"
                 
-                bash ./cli_script.py
+                python3 cli_script.py
 
                 //def displayname = currentBuild.currentExecutor.displayName
                 //def num = currentBuild.currentExecutor.number
